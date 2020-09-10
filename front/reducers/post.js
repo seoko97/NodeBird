619aab83e4c1bb1552e4672a4a6167161b6aeccf
JI <sukho1007@naver.com>
2020-09-10 18:30:18 +0900

    [Add] Reducer

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,55 +1,229 @@
+import produce from 'immer';
+
 export const initialState = {
-	mainPosts: [
-		{
-			User: {
-				id: 1,
-				nickname: 'Sh',
-			},
-			content: '첫번째 게시글',
-			img:
-				'http://sunstat.com/wp-content/uploads/2019/01/%EC%8A%AC%EB%9D%BC%EC%9D%B4%EB%93%9C-%EB%B0%B0%EA%B2%BD%EC%9D%B4%EB%AF%B8%EC%A7%80.png',
-		},
-	],
-	imagepaths: [],
+	mainPosts: [], // 화면에 보일 포스트들
+	imagePaths: [], // 미리보기 이미지 경로
+	addPostErrorReason: false, // 포스트 업로드 실패 사유
+	isAddingPost: false, // 포스트 업로드 중
+	postAdded: false, // 포스트 업로드 성공
+
+	isAddingComment: false, // 댓글 업로드 중
+	commentAdded: false, // 댓글 업로드 성공
+	addCommentErrorReason: '', // 댓글 업로드 실패 사유
+	singlePost: null,
 };
 
-const ADD_POST = 'ADD_POST';
-const ADD_DUMMY = 'ADD_DUMMY';
+export const LOAD_MAIN_POSTS_REQUEST = 'LOAD_MAIN_POSTS_REQUEST';
+export const LOAD_MAIN_POSTS_SUCCESS = 'LOAD_MAIN_POSTS_SUCCESS';
+export const LOAD_MAIN_POSTS_FAILURE = 'LOAD_MAIN_POSTS_FAILURE';
 
-const addPost = {
-	type: ADD_POST,
-};
+export const LOAD_HASHTAG_POSTS_REQUEST = 'LOAD_HASHTAG_POSTS_REQUEST';
+export const LOAD_HASHTAG_POSTS_SUCCESS = 'LOAD_HASHTAG_POSTS_SUCCESS';
+export const LOAD_HASHTAG_POSTS_FAILURE = 'LOAD_HASHTAG_POSTS_FAILURE';
 
-const addDummy = {
-	type: ADD_DUMMY,
-	data: {
-		content: 'Hello',
-		UserId: 1,
-		user: {
-			nickname: 'SH',
-		},
-	},
-};
+export const LOAD_USER_POSTS_REQUEST = 'LOAD_USER_POSTS_REQUEST';
+export const LOAD_USER_POSTS_SUCCESS = 'LOAD_USER_POSTS_SUCCESS';
+export const LOAD_USER_POSTS_FAILURE = 'LOAD_USER_POSTS_FAILURE';
 
-const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case ADD_POST: {
-			return {
-				...state,
-			};
+export const LOAD_IMAGES_POSTS_REQUEST = 'LOAD_IMAGES_POSTS_REQUEST';
+export const LOAD_IMAGES_POSTS_SUCCESS = 'LOAD_IMAGES_POSTS_SUCCESS';
+export const LOAD_IMAGES_POSTS_FAILURE = 'LOAD_IMAGES_POSTS_FAILURE';
+
+// 동기 요청////////////////////////
+export const REMOVE_IMAGE = 'REMOVE_IMAGE';
+
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+export const LIKE_POST_REQUEST = 'LIKE_POST_REQUEST';
+export const LIKE_POST_SUCCESS = 'LIKE_POST_SUCCESS';
+export const LIKE_POST_FAILURE = 'LIKE_POST_FAILURE';
+
+export const UNLIKE_POST_REQUEST = 'UNLIKE_POST_REQUEST';
+export const UNLIKE_POST_SUCCESS = 'UNLIKE_POST_SUCCESS';
+export const UNLIKE_POST_FAILURE = 'UNLIKE_POST_FAILURE';
+
+export const ADD_COMMENTS_REQUEST = 'ADD_COMMENTS_REQUEST';
+export const ADD_COMMENTS_SUCCESS = 'ADD_COMMENTS_SUCCESS';
+export const ADD_COMMENTS_FAILURE = 'ADD_COMMENTS_FAILURE';
+
+export const LOAD_COMMENTS_REQUEST = 'LOAD_COMMENTS_REQUEST';
+export const LOAD_COMMENTS_SUCCESS = 'LOAD_COMMENTS_SUCCESS';
+export const LOAD_COMMENTS_FAILURE = 'LOAD_COMMENTS_FAILURE';
+
+export const RETWEET_REQUEST = 'RETWEET_REQUEST';
+export const RETWEET_SUCCESS = 'RETWEET_SUCCESS';
+export const RETWEET_FAILURE = 'RETWEET_FAILURE';
+
+export const REMOVE_POST_REQUEST = 'REMOVE_POST_REQUEST';
+export const REMOVE_POST_SUCCESS = 'REMOVE_POST_SUCCESS';
+export const REMOVE_POST_FAILURE = 'REMOVE_POST_FAILURE';
+
+export const UPLOAD_IMAGES_REQUEST = 'UPLOAD_IMAGES_REQUEST';
+export const UPLOAD_IMAGES_SUCCESS = 'UPLOAD_IMAGES_SUCCESS';
+export const UPLOAD_IMAGES_FAILURE = 'UPLOAD_IMAGES_FAILURE';
+
+export const LOAD_POST_REQUEST = 'LOAD_POST_REQUEST';
+export const LOAD_POST_SUCCESS = 'LOAD_POST_SUCCESS';
+export const LOAD_POST_FAILURE = 'LOAD_POST_FAILURE';
+
+export default (state = initialState, action) => {
+	return produce(state, (draft) => {
+		switch (action.type) {
+			// 이미지 업로드
+			case UPLOAD_IMAGES_REQUEST: {
+				break;
+			}
+			case UPLOAD_IMAGES_SUCCESS: {
+				action.data.forEach((p) => {
+					draft.imagePaths.push(p);
+				});
+				break;
+			}
+			case UPLOAD_IMAGES_FAILURE: {
+				break;
+			}
+
+			case REMOVE_IMAGE: {
+				const index = draft.imagePaths.findIndex((v, i) => i === action.index);
+				draft.imagePaths.splice(index, 1);
+				break;
+			}
+
+			// 게시글 정보 저장
+			case ADD_POST_REQUEST: {
+				draft.isAddingPost = true;
+				draft.addPostErrorReason = '';
+				draft.postAdded = false;
+				break;
+			}
+			case ADD_POST_SUCCESS: {
+				draft.isAddingPost = false;
+				// 기존 포스트 앞쪽에 생성된 포스트 저장
+				draft.mainPosts.unshift(action.data);
+				draft.postAdded = true;
+				draft.imagePaths = [];
+				break;
+			}
+			case ADD_POST_FAILURE: {
+				draft.isAddingPost = false;
+				draft.addPostErrorReason = action.error;
+				break;
+			}
+
+			// 댓글 정보 저장
+			case ADD_COMMENTS_REQUEST: {
+				draft.isAddingComment = true;
+				draft.addCommentErrorReason = '';
+				draft.commentAdded = false;
+				break;
+			}
+			case ADD_COMMENTS_SUCCESS: {
+				// draft 를 통해 기존에 있던 게시글에서 입력된 data값에 저장된 id 값과 같은 게시글 검색
+				const postIndex = draft.mainPosts.findIndex((v) => v.id === action.data.postId);
+				draft.mainPosts[postIndex].Comments.push(action.data.comment);
+				draft.isAddingComment = false;
+				draft.commentAdded = true;
+				break;
+			}
+			case ADD_COMMENTS_FAILURE: {
+				draft.isAddingComment = false;
+				draft.addCommentErrorReason = action.error;
+				break;
+			}
+
+			case LOAD_COMMENTS_SUCCESS: {
+				const postIndex = draft.mainPosts.findIndex((v) => v.id === action.data.postId);
+				draft.mainPosts[postIndex].Comments = action.data.comments;
+				break;
+			}
+
+			// 게시글 불러오기
+			case LOAD_MAIN_POSTS_REQUEST:
+			// 다른 유저 게시글 불러오기
+			case LOAD_USER_POSTS_REQUEST:
+			// 해당 해시태그 게시글 불러오기
+			case LOAD_HASHTAG_POSTS_REQUEST: {
+				draft.mainPosts = !action.lastId ? [] : draft.mainPosts;
+				draft.hasMorePost = action.lastId ? draft.hasMorePost : true;
+				break;
+			}
+			case LOAD_MAIN_POSTS_SUCCESS:
+			case LOAD_USER_POSTS_SUCCESS:
+			case LOAD_HASHTAG_POSTS_SUCCESS: {
+				action.data.forEach((d) => {
+					draft.mainPosts.push(d);
+				});
+				draft.hasMorePost = action.data.length === 10;
+				break;
+			}
+			case LOAD_MAIN_POSTS_FAILURE:
+			case LOAD_USER_POSTS_FAILURE:
+			case LOAD_HASHTAG_POSTS_FAILURE: {
+				break;
+			}
+
+			// 좋아요
+			case LIKE_POST_REQUEST: {
+				break;
+			}
+			case LIKE_POST_SUCCESS: {
+				const postIndex = draft.mainPosts.findIndex((v) => v.id === action.data.postId);
+				draft.mainPosts[postIndex].Likers.unshift({ id: action.data.userId });
+				break;
+			}
+			case LIKE_POST_FAILURE: {
+				break;
+			}
+
+			// 좋아요 취소
+			case UNLIKE_POST_REQUEST: {
+				break;
+			}
+			case UNLIKE_POST_SUCCESS: {
+				const postIndex = draft.mainPosts.findIndex((v) => v.id === action.data.postId);
+				const likeIndex = draft.mainPosts[postIndex].Likers.findIndex(
+					(v) => v.id === action.data.userId,
+				);
+				draft.mainPosts[postIndex].Likers.splice(likeIndex, 1);
+				break;
+			}
+			case UNLIKE_POST_FAILURE: {
+				break;
+			}
+
+			// 리트윗
+			case RETWEET_REQUEST: {
+				break;
+			}
+			case RETWEET_SUCCESS: {
+				draft.mainPosts.unshift(action.data);
+				break;
+			}
+			case RETWEET_FAILURE: {
+				break;
+			}
+
+			// 포스트 삭제
+			case REMOVE_POST_REQUEST: {
+				break;
+			}
+			case REMOVE_POST_SUCCESS: {
+				const index = draft.mainPosts.filter((v) => v.id === action.data);
+				draft.mainPosts.splice(index, 1);
+				break;
+			}
+			case REMOVE_POST_FAILURE: {
+				break;
+			}
+			case LOAD_POST_SUCCESS: {
+				draft.singlePost = action.data;
+				break;
+			}
+			default: {
+				break;
+			}
 		}
-		case ADD_DUMMY: {
-			return {
-				...state,
-				mainPosts: [action.data, ...state.mainPosts],
-			};
-		}
-		default: {
-			return {
-				...state,
-			};
-		}
-	}
+	});
 };
-
-export default reducer;
--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,68 +1,251 @@
-//user 정보들
-const dummyUser = {
-	nickname: '지석호',
-	Post: [],
-	Followings: [],
-	Followers: [],
-	signUpData: {},
-};
+import produce from 'immer';
 
+// user 정보들
 export const initialState = {
-	//초기값
-	user: null,
-	isLoggedIn: false,
+	// 초기값
+	isLoggingIn: false, // 로그인 시도중
+	logInErrorReason: '', // 로그인 실패 사유
+
+	isLoggingOut: false, // 로그아웃 시도중
+
+	signUp: false, // 회원가입 성공
+	isSigningUp: false, // 회원 가입 시도중
+	isSignedUp: false,
+	signUpErrorReaon: '', // 회원가입 실패 사유
+
+	me: null, // 내정보
+	followingList: [], // 팔로잉 리스트
+	followerList: [], // 팔로워 리스트
+	userInfo: null, // 남의 정보
+
+	isEditingNickname: false, // 닉네임 변경중
+	editNicknameErrorReason: '', // 닉네임 변경 실패 사유
+	hasMoreFollower: false,
+	hasMoreFollowing: false,
 };
 
-//액션 이름
-export const LOG_IN = 'LOG_IN';
+// 액션 이름
+// 비동기 요청에 경우 해당 이름을 사용하자 (REQUEST, SUCCESS, FAILURE)
+// 동기요청은 바로 실행되기 때문에 해당 단어를 사용하지 말자
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
-export const LOG_IN_FAILRE = 'LOG_IN_FAILRE';
-export const LOG_OUT = 'LOG_OUT';
-export const SIGN_UP = 'SIGN_UP';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+
+export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
 
-//액션의 이름에 따라 저장될 정보를 저장
-export const loginAction = {
-	type: LOG_IN,
-};
-export const logoutAction = {
-	type: LOG_OUT,
-};
-export const signUpAction = (data) => {
-	return { type: SIGN_UP, data: data };
-};
+export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
+export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
 
-const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case LOG_IN: {
-			return {
-				//새로운 객체를 먼저 생성하고 현재 정보를 불러와 저장
-				...state,
-				isLoggedIn: true,
-				user: dummyUser,
-			};
-		}
-		case LOG_OUT: {
-			return {
-				//새로운 객체를 먼저 생성하고 현재 정보를 불러와 저장
-				...state,
-				isLoggedIn: false,
-				user: {},
-			};
-		}
-		case SIGN_UP: {
-			return {
-				...state,
-				signUpData: action.data,
-			};
-		}
+export const LOAD_USER_REQUEST = 'LOAD_USER_REQUEST';
+export const LOAD_USER_SUCCESS = 'LOAD_USER_SUCCESS';
+export const LOAD_USER_FAILURE = 'LOAD_USER_FAILURE';
 
-		default: {
-			return {
-				...state,
-			};
+export const LOAD_FOLLOWERS_REQUEST = 'LOAD_FOLLOWERS_REQUEST';
+export const LOAD_FOLLOWERS_SUCCESS = 'LOAD_FOLLOWERS_SUCCESS';
+export const LOAD_FOLLOWERS_FAILURE = 'LOAD_FOLLOWERS_FAILURE';
+
+export const LOAD_FOLLOWINGS_REQUEST = 'LOAD_FOLLOWINGS_REQUEST';
+export const LOAD_FOLLOWINGS_SUCCESS = 'LOAD_FOLLOWINGS_SUCCESS';
+export const LOAD_FOLLOWINGS_FAILURE = 'LOAD_FOLLOWINGS_FAILURE';
+
+export const FOLLOW_USER_REQUEST = 'FOLLOW_USER_REQUEST';
+export const FOLLOW_USER_SUCCESS = 'FOLLOW_USER_SUCCESS';
+export const FOLLOW_USER_FAILURE = 'FOLLOW_USER_FAILURE';
+
+export const UNFOLLOW_USER_REQUEST = 'UNFOLLOW_USER_REQUEST';
+export const UNFOLLOW_USER_SUCCESS = 'UNFOLLOW_USER_SUCCESS';
+export const UNFOLLOW_USER_FAILURE = 'UNFOLLOW_USER_FAILURE';
+
+export const REMOVE_FOLLOWER_REQUEST = 'REMOVE_FOLLOWER_REQUEST';
+export const REMOVE_FOLLOWER_SUCCESS = 'REMOVE_FOLLOWER_SUCCESS';
+export const REMOVE_FOLLOWER_FAILURE = 'REMOVE_FOLLOWER_FAILURE';
+
+export const EDIT_NICKNAME_REQUEST = 'EDIT_NICKNAME_REQUEST';
+export const EDIT_NICKNAME_SUCCESS = 'EDIT_NICKNAME_SUCCESS';
+export const EDIT_NICKNAME_FAILURE = 'EDIT_NICKNAME_FAILURE';
+
+export const ADD_POST_TO_ME = 'ADD_POST_TO_ME';
+export const REMOVE_POST_OF_ME = 'REMOVE_POST_OF_ME';
+
+export default (state = initialState, action) => {
+	return produce(state, (draft) => {
+		switch (action.type) {
+			// 로그인 요청
+			case LOG_IN_REQUEST: {
+				draft.isLoggingIn = true;
+				draft.logInErrorReason = '';
+				break;
+			}
+			case LOG_IN_SUCCESS: {
+				draft.isLoggingIn = false;
+				draft.me = action.data;
+				draft.logInErrorReason = '';
+				break;
+			}
+			case LOG_IN_FAILURE: {
+				draft.isLoggingIn = false;
+				draft.logInErrorReason = action.reason;
+				draft.me = null;
+				break;
+			}
+
+			// 로그아웃 요청
+			case LOG_OUT_REQUEST: {
+				draft.isLoggingOut = true;
+				break;
+			}
+			case LOG_OUT_SUCCESS: {
+				draft.isLoggingOut = false;
+				draft.me = null;
+				break;
+			}
+
+			// 회원가입 요청
+			case SIGN_UP_REQUEST: {
+				draft.isSignedUp = false;
+				draft.isSigningUp = true;
+				draft.signUpErrorReaon = '';
+				break;
+			}
+			case SIGN_UP_SUCCESS: {
+				draft.isSignedUp = true;
+				draft.isSigningUp = false;
+				break;
+			}
+			case SIGN_UP_FAILURE: {
+				draft.isSigningUp = false;
+				draft.signUpErrorReaon = action.error;
+				break;
+			}
+
+			// 포스트 불러오기 요청
+			case LOAD_USER_REQUEST: {
+				break;
+			}
+			case LOAD_USER_SUCCESS: {
+				if (action.me) {
+					draft.me = action.data;
+					break;
+				}
+				draft.userInfo = action.data;
+				break;
+			}
+			case LOAD_USER_FAILURE: {
+				break;
+			}
+
+			// 팔로우
+			case FOLLOW_USER_REQUEST: {
+				break;
+			}
+			case FOLLOW_USER_SUCCESS: {
+				draft.me.Followings.unshift({ id: action.data });
+				break;
+			}
+			case FOLLOW_USER_FAILURE: {
+				break;
+			}
+
+			// 언팔로우
+			case UNFOLLOW_USER_REQUEST: {
+				break;
+			}
+			case UNFOLLOW_USER_SUCCESS: {
+				const index = draft.me.Followings.findIndex((v) => v.id === action.data);
+				draft.me.Followings.splice(index, 1);
+				const index2 = draft.followingList.findIndex((v) => v.id === action.data);
+				draft.followingList.splice(index2, 1);
+				break;
+			}
+			case UNFOLLOW_USER_FAILURE: {
+				break;
+			}
+
+			// 포스트 정보 가져오기
+			case ADD_POST_TO_ME: {
+				draft.me.Posts.unshift({ id: action.data });
+				break;
+			}
+
+			// 포스트 정보 변경(삭제)
+			case REMOVE_POST_OF_ME: {
+				const index = draft.me.Posts.findIndex((v) => v.id === action.data);
+				draft.me.Posts.splice(index, 1);
+				break;
+			}
+
+			// 내 팔로워 목록 불러우기
+			case LOAD_FOLLOWERS_REQUEST: {
+				draft.followerList = !action.offset ? [] : draft.followerList;
+				draft.hasMoreFollower = action.offset ? draft.hasMoreFollower : true;
+				break;
+			}
+			case LOAD_FOLLOWERS_SUCCESS: {
+				action.data.forEach((d) => {
+					draft.followerList.push(d);
+				});
+				draft.hasMoreFollower = action.data.length === 3;
+				break;
+			}
+			case LOAD_FOLLOWERS_FAILURE: {
+				break;
+			}
+
+			// 날 팔로잉한 유저 불러오기
+			case LOAD_FOLLOWINGS_REQUEST: {
+				draft.followingList = !action.offset ? [] : draft.followingList;
+				draft.hasMoreFollowing = action.offset ? draft.hasMoreFollowing : true; // 처음 데이터를 가져올 때는 더보기 버튼을 보여주는 걸로
+				break;
+			}
+			case LOAD_FOLLOWINGS_SUCCESS: {
+				action.data.forEach((d) => {
+					draft.followingList.push(d);
+				});
+				draft.hasMoreFollowing = action.data.length === 3;
+				break;
+			}
+			case LOAD_FOLLOWINGS_FAILURE: {
+				break;
+			}
+
+			// 팔로워 삭제하기
+			case REMOVE_FOLLOWER_REQUEST: {
+				break;
+			}
+			case REMOVE_FOLLOWER_SUCCESS: {
+				const index = draft.me.Followers.findIndex((v) => v.id === action.data);
+				draft.me.Followers.splice(index, 1);
+				const index2 = draft.followerList.findIndex((v) => v.id === action.data);
+				draft.followerList.splice(index2, 1);
+				break;
+			}
+			case REMOVE_FOLLOWER_FAILURE: {
+				break;
+			}
+
+			// 닉네임 변경
+			case EDIT_NICKNAME_REQUEST: {
+				draft.isEditingNickname = true;
+				draft.editNicknameErrorReason = '';
+				break;
+			}
+			case EDIT_NICKNAME_SUCCESS: {
+				draft.isEditingNickname = false;
+				draft.me.nickname = action.data;
+				break;
+			}
+			case EDIT_NICKNAME_FAILURE: {
+				draft.isEditingNickname = false;
+				draft.editNicknameErrorReason = action.error;
+				break;
+			}
+
+			default: {
+				break;
+			}
 		}
-	}
+	});
 };
-
-export default reducer;
